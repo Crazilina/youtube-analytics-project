56f72efaea7e87fd18c4030db06a1e239e149d6f
Alina Salakhova <alina.salakhova1991@gmail.com>
2024-01-15 01:20:46 +0300

    Добавлен класс Channel с методом print_info для вывода информации о YouTube канале

--- src/channel.py
+++ src/channel.py
@@ -1,3 +1,11 @@
+import os
+import json
+from googleapiclient.discovery import build
+
+
+def printj(dict_to_print: dict) -> None:
+    """Выводит словарь в json-подобном удобном формате с отступами"""
+    print(json.dumps(dict_to_print, indent=2, ensure_ascii=False))
 
 
 class Channel:
@@ -5,8 +13,22 @@ class Channel:
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        pass
+        self.channel_id = channel_id
+        self.api_key = os.getenv('YT_API_KEY')
+        self.youtube = build('youtube', 'v3', developerKey=self.api_key)
+        self.channel_data = self.get_channel_data()
+
+    def get_channel_data(self):
+        """Получает полные данные о канале с YouTube API, включая общую структуру ответа."""
+        request = self.youtube.channels().list(
+            id=self.channel_id,
+            part='snippet,statistics'
+        )
+        return request.execute()
 
     def print_info(self) -> None:
-        """Выводит в консоль информацию о канале."""
-        pass
+        """Выводит в консоль полную информацию о канале."""
+        if self.channel_data:
+            printj(self.channel_data)
+        else:
+            print("Channel information not found.")
